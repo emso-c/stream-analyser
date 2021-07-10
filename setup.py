1cec24452050578326e12df9b134c45d7f7d0487
emso-c <32599085+emso-c@users.noreply.github.com>
2021-07-10 22:55:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = "stream-analyser",
-    version = "v0.0.1",
+    version = "v0.1.0",
     author = "emso-c",
     author_email = "emsoc192@gmail.com",
     description = ("Live stream analyser"),
