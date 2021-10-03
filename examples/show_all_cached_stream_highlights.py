a563df7c29fd79e3cb48420397e7a5a21a73f5c3
emso-c <32599085+emso-c@users.noreply.github.com>
2021-10-03 11:33:33 +0200

    Rename examples

--- examples/show_all_cached_stream_highlights.py
+++ examples/show_all_cached_stream_highlights.py
@@ -5,9 +5,12 @@ from modules.filehandler import streamanalyser_filehandler
 # Find highlights for streams that are cached
 if __name__ == "__main__":
 
+    # Limited to merely 1000 messages for this example as it'd take way longer to fetch all messages.
+    message_limit = 1000
+
     # Another option would be creating an empty StreamAnalyser instance to get the cached ids
     for id in streamanalyser_filehandler.get_cached_ids():
-        with sa.StreamAnalyser(id, msglimit=1000) as analyser:
+        with sa.StreamAnalyser(id, msglimit=message_limit) as analyser:
             analyser.analyse()
             analyser.get_highlights(
                 top=len(analyser.highlights), output_mode="detailed"
