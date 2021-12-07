f55b94eb9abe824d14aa63e212ee8a464cf6def1
emso-c <32599085+emso-c@users.noreply.github.com>
2021-12-07 23:46:48 +0100

    Adjust custom_context example

--- examples/custom_context.py
+++ examples/custom_context.py
@@ -10,10 +10,15 @@ if __name__ == "__main__":
         not_cache=True,
         disable_logs=True,
         msglimit=1000,
+        default_context_path=None # set default context path to None to disable premade default contexts
     )
     with analyser_obj as analyser:
+        # context source paths should be absolute paths
         analyser.context_source.add(os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "contexts_example.json"
         ))
+
+        # context sources should be added before the analyse function
         analyser.analyse()
+
         analyser.get_highlights(output_mode="detailed")
