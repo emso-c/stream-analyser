359dee3baaffdd2f7b6aee8d5a49e05ca9314c79
emso-c <32599085+emso-c@users.noreply.github.com>
2021-12-08 10:31:20 +0100

    Update example

--- examples/show_all_cached_stream_highlights.py
+++ examples/show_all_cached_stream_highlights.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     # Limited to merely 1000 messages for this example as it'd take way longer to fetch all messages.
     message_limit = 1000
 
-    # Another option would be creating an empty StreamAnalyser instance to get the cached ids
+    # Creating an empty analyser object instance to get the cached ids
     for id in sa.StreamAnalyser("").filehandler.get_cached_ids():
         with sa.StreamAnalyser(id, msglimit=message_limit) as analyser:
             analyser.analyse()
