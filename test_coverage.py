4bec52444e40f81a60fedf16c19e7657bd467d79
emso-c <32599085+emso-c@users.noreply.github.com>
2022-03-03 13:33:31 +0300

    Update test folder path

--- test_coverage.py
+++ test_coverage.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     cov = coverage.Coverage()
 
     cov.start()
-    suite = unittest.TestLoader().discover("streamanalyser/test", pattern="*")
+    suite = unittest.TestLoader().discover("streamanalyser/tests", pattern="*")
     unittest.TextTestRunner().run(suite)
     cov.stop()
 
