5352c88457b7db8739a3e67bc7fdfa7d89ae9ac7
emso-c <32599085+emso-c@users.noreply.github.com>
2022-03-12 01:02:17 +0300

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="stream-analyser",
-    version="v0.3.4",
+    version="v0.3.5",
     author="emso-c",
     author_email="emsoc192@gmail.com",
     description=("A tool that analyses live streams"),
